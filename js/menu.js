554e847daf74119734eb92d682315aaf497ff536
Jose Ismael Castro Ruiz <joseismaelcastroruiz@gmail.com>
2020-10-22 13:06:21 -0600

    my first web page

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+const hamburguer = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu-navegacion')
+
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e =>{
+    if(menu.classList.contains('spread') 
+        && e.target != menu && e.target != hamburguer){
+        console.log('cerrar')
+        menu.classList.toggle("spread")
+    }
+})
+
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.contenedor-servicio');
+ScrollReveal().reveal('.contenedor-galeria');
+ScrollReveal().reveal('.gallery');
+ScrollReveal().reveal('.contenedor');
+ScrollReveal().reveal('.experts');
+ScrollReveal().reveal('.footer');
