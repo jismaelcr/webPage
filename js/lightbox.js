f5649e0729bebd14916e66d8d1a901a871afcac8
jismaelcr <45930067+jismaelcr@users.noreply.github.com>
2021-09-08 15:51:10 -0600

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,7 +1,9 @@
 const imagenes = document.querySelectorAll('.img-galeria')
 const imagenLight = document.querySelector('.agregar-imagen');
+const videoLight = document.querySelector('.agregar-video');
 const contenedorLight = document.querySelector('.imagen-light')
-const closeLight = document.querySelector('.close')
+const contenedorVideoLight = document.querySelector('.video-light')
+
 
 
 imagenes.forEach(imagen => {
@@ -17,11 +19,28 @@ contenedorLight.addEventListener('click',(e)=>{
         hamburguer.style.opacity = '1';
     }
 })
+contenedorVideoLight.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight){
+        contenedorVideoLight.classList.toggle('show')
+        videoLight.classList.toggle('showVideo')
+        hamburguer.style.opacity = '1';
+    }
+})
 
 
 const aparecerImagen = (imagen)=>{
-    imagenLight.src = imagen;
-    contenedorLight.classList.toggle('show')
-    imagenLight.classList.toggle('showImage')
-    hamburguer.style.opacity = '0';
+    //console.log(imagen.indexOf("jpg"));
+    if(imagen.indexOf("jpg")===9){
+        imagenLight.src = imagen;
+        videoLight.src="";
+        contenedorLight.classList.toggle('show')
+        imagenLight.classList.toggle('showImage')
+        hamburguer.style.opacity = '0';
+    }else{
+        imagenLight.src ="";
+        videoLight.src = imagen;
+        contenedorVideoLight.classList.toggle('show')
+        videoLight.classList.toggle('showVideo')
+        hamburguer.style.opacity = '0';
+    }
 }
